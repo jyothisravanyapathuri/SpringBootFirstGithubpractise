f7db43762ecebf903eb3e7b1b232776fe6073d08
dileep6522 <dileepnandigam4@gmail.com>
2026-03-13 15:57:26 +0530

    Intial springboot application

---
+++ src/main/java/com/example/demo/test/ControllerClass.java
@@ -0,0 +1,15 @@
+package com.example.demo.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerClass {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		System.out.println("....in msg controller...");
+		return "good morning";
+
+	}
+}
---
+++ src/main/java/com/example/demo/test/TestClass.java
@@ -0,0 +1,12 @@
+package com.example.demo.test;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestClass {
+	
+	TestClass(){
+		System.out.println(".....testclass....");
+	}
+
+}
